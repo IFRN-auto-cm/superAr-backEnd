de5f7d3b47bdc249670754b2e52fa10151b7ecc8
Lennedy <lennedy.soares@ifrn.edu.br>
2026-06-21 17:40:34 -0300

    Colocado o host do banco no .env

--- api.py
+++ api.py
@@ -66,8 +66,7 @@ def publicar_mqtt(endereco, payload):
 
 def get_db():
     return MySQLdb.connect(
-        # host="127.0.0.1",
-        host="mysql_super_ar",
+        host= os.getenv("HOST"),
         user= os.getenv("MYSQL_USER"),
         passwd= os.getenv("MYSQL_PASSWORD"),
         db= os.getenv("MYSQL_DATABASE"),
