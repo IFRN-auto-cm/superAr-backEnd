7734488382c2bd1b0e34331c22d920052027aef7
Ricardo0226 <144167797+Ricardo0226@users.noreply.github.com>
2026-09-24 11:29:10 -0300

    primeira regra separada no service.

--- ar_condicionado/service.py
+++ ar_condicionado/service.py
@@ -1,3 +1,4 @@
+from repository import atualizar_ar 
 import mqtt_service_client
 
 def publicar_mqtt(endereco, payload):
@@ -5,3 +6,42 @@ def publicar_mqtt(endereco, payload):
         atuador=endereco,
         payload=payload
     )
+
+def atualizar_ar(ar_cadastrado_id, data):
+
+    temperatura_medida = 0
+    temperatura_referencia = data.get("temperatura_referencia")
+    modelo_marca = data.get("marcaModeloId")
+    status = data.get("status")
+    atuador = data.get("atuador")
+    nome = data.get("nome")
+    sala = data.get("sala")
+
+    if not modelo_marca:
+        raise ValueError(
+            "modelo_marca é obrigatório"
+        )
+
+    if not sala:
+        raise ValueError(
+            "sala é obrigatória"
+        )
+
+    linhas_afetadas = repository_atualizar_ar(
+        ar_cadastrado_id=ar_cadastrado_id,
+        temperatura_medida=temperatura_medida,
+        temperatura_referencia=temperatura_referencia,
+        modelo_marca=modelo_marca,
+        status=status,
+        atuador=atuador,
+        nome=nome,
+        sala=sala
+    )
+
+    return linhas_afetadas
+
+
+
+
+
+
